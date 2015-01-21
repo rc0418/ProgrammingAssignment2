fbb6a9c198dbcedda340d0862d97153e7542b6cb
rc0418 <ronnie_cowl@hotmail.com>
2015-01-21 19:46:53 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-##makeCacheMatrix is a function allowing you to create and set a matrix as well as its inverse - stored as variable 'm'. 
+##makeCacheMatrix is a function allowing you to create and set a matrix as well as getting and setting its inverse 
+# - stored as variable 'm'. 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,8 +13,8 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix = setmatrix,
        getmatrix = getmatrix)
 }
-##cacheSolve inverts the matrix. If the inverse matrix is already available, it will be returned without the need of computation.
-##If the inverse matrix is not found, it is computed and then returned.  
+##cacheSolve returns the inverted matrix. If the inverse matrix is already available, it will be returned without the need of 
+##computation. If the inverse matrix is not found, it is computed and then returned.  
 cacheSolve <- function(x, ...) {
   m <- x$getmatrix()
   if(!is.null(m)) {
