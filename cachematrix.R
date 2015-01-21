43283e719973a3311ed70dbed9d3be66cdadd805
rc0418 <ronnie_cowl@hotmail.com>
2015-01-21 19:56:27 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+##Often, when using long vectors or using multiple loops, functions can take a long time to execute.
+##The purpose of these functions are to save computing time by caching known values. Therefore, when the next part of the 
+##function is executed and the needed value is the same as the stored value, cached value is used, instead of being recomputed.
+##These funcitons also compare the required value to the cached value, recomputing the value if they are not the same. 
+
 ##makeCacheMatrix is a function allowing you to create and set a matrix as well as getting and setting its inverse 
 # - stored as variable 'm'. 
 makeCacheMatrix <- function(x = matrix()) {
